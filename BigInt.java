94dd89216c00e494183a2d5ce6078e592eab2031
Collin Arnett <collin@arnett.it>
2018-06-07 23:09:28 -0400

    Added funtionality for positive addition.

--- BigInt.java
+++ BigInt.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -19,8 +20,8 @@ ArrayList<Integer> bigNum = new ArrayList<Integer>();
 // =============================================================================
 
 public BigInt(){
-        isNegative = true;
-        ArrayList<Integer> bigNum = bigNum.add(0);
+        isNegative = false;
+        this.bigNum.add(0);
 }
 
 public BigInt(String aNumber){
@@ -28,9 +29,8 @@ public BigInt(String aNumber){
 
         if (isNegative == true || plusSignDetected(aNumber) == true) {
                 aNumber = removeSign(aNumber);
-                if (isInteger(aNumber) || whiteSpaceDetected(aNumber)) {
+                if (isInteger(aNumber) && whiteSpaceDetected(aNumber) == false) {
                         createArrayList(aNumber);
-
                 }
                 else{
                         throw new IsNotInteger("Please remove special characters");
@@ -38,7 +38,7 @@ public BigInt(String aNumber){
         }
 
         else {
-                if (isInteger(aNumber) || whiteSpaceDetected(aNumber)) {
+                if (isInteger(aNumber) && whiteSpaceDetected(aNumber) == false) {
                         createArrayList(aNumber);
                 }
                 else {
@@ -47,15 +47,71 @@ public BigInt(String aNumber){
         }
 }
 
+
 @Override
 public String toString(){
-        ArrayList<Integer> temp = bigNum;
+        ArrayList<Integer> temp = this.bigNum;
+        String s = new String();
 
         Collections.reverse(temp);
 
-        String s = new String();
-        return s = temp.stream().map(Object:: toString).collect(Collectors.joining(""));
+      s = temp.stream().map(Object:: toString).collect(Collectors.joining(""));
+      return  s.replaceFirst("^0+(?!$)", "");
 
+}
+
+public BigInt add(BigInt other){
+int numberOfZeros = 0;
+
+// check and add zeros if needed
+        if (this.bigNum.size()-1 > other.bigNum.size()-1) {
+                numberOfZeros = this.bigNum.size() - other.bigNum.size();
+                for (int i=0; i < numberOfZeros; i++) {
+                        other.bigNum.add(0);
+                }
+        }
+        else if (other.bigNum.size()-1 > this.bigNum.size()-1) {
+                numberOfZeros = other.bigNum.size() - this.bigNum.size();
+                for (int i=0; i < numberOfZeros; i++) {
+                        this.bigNum.add(0);
+                }
+        }
+
+
+        ArrayList<Integer> sum = createSum(createTempArrayList(this, other));
+        BigInt result = new BigInt(sum.toString().replaceAll("[\\[\\], ]", ""));
+        return result;
+}
+
+private ArrayList<Integer> createTempArrayList(BigInt aNumber, BigInt other ){
+
+        ArrayList<Integer> tempArrayList = new ArrayList<Integer>();
+        int tempInt = 0;
+        for(int i = (aNumber.bigNum.size()-1); i >=0; i--) {
+                tempInt = aNumber.bigNum.get(i) + other.bigNum.get(i);
+                System.out.println(tempInt);
+                tempArrayList.add(tempInt);
+                tempInt = 0;
+        }
+        return tempArrayList;
+}
+
+private ArrayList<Integer> createSum(ArrayList<Integer> tempArrayList){
+        for(int i = (tempArrayList.size()-1); i >= 1; i--) {
+                if (tempArrayList.get(i) > 9) {
+                        tempArrayList.set(i,tempArrayList.get(i) - 10);
+                        tempArrayList.set(i - 1,tempArrayList.get(i - 1) + 1 );
+                        System.out.println(tempArrayList.get(i));
+                }
+
+        }
+        System.out.println(tempArrayList.get(0));
+        if (tempArrayList.get(0) > 9) {
+                tempArrayList.set(0,tempArrayList.get(0) - 10);
+                tempArrayList.add(0,1);
+
+        }
+        return tempArrayList;
 }
 
 // =============================================================================
@@ -65,11 +121,11 @@ public String toString(){
 private void isNegative(String aNumber){
         if(aNumber.charAt(0) == '-') {
                 isNegative = true;
-                System.out.println("isNegative = true");
+                //  System.out.println("isNegative = true");
         }
         else {
                 isNegative = false;
-                System.out.println("isNegative = false");
+                //  System.out.println("isNegative = false");
         }
 }
 
@@ -135,25 +191,19 @@ private String removeSign(String aNumber){
 // TODO Future Code
 // =============================================================================
 
-// private addNumbers(BigInt other){
-//         if (other.bigNum && this.bigNum == isNegative || other.bigNum && this.bigNum == isNegative) {
-//                 add(this.bigNum, other.bigNum);
-//         }
+//  private addNumbers(BigInt other){
 //
-//         elseif (this.bigNum>=other.bigNum){
-//                 subtract(this.bigNum, other.bigNum);
-//
-//                 else{
-//                         stubtract(other.bigNum, this.bigNum);
-//                 }
-//         }
-// }
-//
-// Greatest absolute value to calculate sign at the end
-// }
-// public multiplyNumbers(){
-//
-// }
+//          if (other.bigNum && this.bigNum == (isNegative == true) || other.bigNum && this.bigNum == (isNegative == false) ) {
+//                  add(this.bigNum, other.bigNum);
+//          }
+//          else if (this.bigNum >= other.bigNum){
+//                  subtract(this.bigNum, other.bigNum);
+//          }
+//          else{
+//                  stubtract(other.bigNum, this.bigNum);
+//          }
+//  }
+
 //
 // public divideNumbers(){
 //
